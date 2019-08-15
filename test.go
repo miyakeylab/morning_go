03d210a3fdeec81bb77a6e8b082bf199db8ca0c4
miyakeylab <miyasaviola@gmail.com>
2019-08-15 09:19:51 +0900

    capテスト

--- test.go
+++ test.go
@@ -21,6 +21,25 @@ func main() {
 	}
 
 	typedata.Typedata()
+
+	ns := []int{10, 20, 30, 40, 50} // 要素にアクセス
+	fmt.Println(cap(ns))
+	fmt.Println(ns[3])
+	// 長さ
+	fmt.Println(len(ns))
+	// 要素􏰁追加
+	// 容量が足りない場合􏰀背後􏰁配列が再確保される
+	ns = append(ns, 60, 70)
+	// 容量
+	fmt.Println(cap(ns))
+
+	ns = append(ns, 60, 70)
+
+	fmt.Println(cap(ns))
+
+	ns = append(ns, 60, 70)
+
+	fmt.Println(cap(ns))
 }
 
 func swap(x, y int) (int, int) {
